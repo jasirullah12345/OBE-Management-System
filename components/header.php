c00ca5a9cd3a9ebb3864d7e7113cfbc6d3480f2f
Ali Usama <101451293+Ali0548@users.noreply.github.com>
2022-03-13 10:50:37 +0500

    Login Page

--- components/header.php
+++ components/header.php
@@ -6,10 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OBE - Management System</title>
     <link rel="shortcut icon" href="../assets/images/logo.ico" type="image/x-icon"/>
+    
 
-    <!--    Boostrap CDN-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-            crossorigin="anonymous"></script>
+   
 </head>
 <body>
