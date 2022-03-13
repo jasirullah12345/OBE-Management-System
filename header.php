6a327e156bc5c4bde6cc15cec795f74790f349c2
Jasir Ullah Khan <jasirullah12345@gmail.com>
2022-03-13 15:41:41 +0500

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OBE - Management System</title>
-    <link rel="shortcut icon" href="../assets/images/logo.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="assets/images/logo.ico" type="image/x-icon"/>
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
