bbbcba52106a41b51b3465d92bb352516add325d
Jasir Ullah Khan <jasirullah12345@gmail.com>
2022-03-15 20:16:20 +0500

    user update and display gui

--- user.php
+++ user.php
@@ -41,12 +41,56 @@ include("header.php");
                     <!-- left column -->
                     <div class="col-md-12">
                         <!-- jquery validation -->
-                        <div class="card card-primary">
+                        <div class="card <?php
+                        if (isset($_GET['edit']) && $_GET['edit'] == "true") {
+                            echo "card-warning";
+                        } else {
+                            echo "card-primary";
+                        }
+                        ?>">
                             <div class="card-header">
-                                <h3 class="card-title">Add New User </h3>
+                                <h3 class="card-title">
+                                    <?php
+                                    if (isset($_GET['edit']) && $_GET['edit'] == "true") {
+                                        echo "Edit User";
+                                    } else {
+                                        echo "Add New User";
+                                    }
+                                    ?>
+                                     </h3>
                             </div>
-                            <!-- /.card-header -->
-                            <!-- form start -->
+                            <?php
+                            if (isset($_GET['edit']) && $_GET['edit'] == "true") {
+                                echo '<!-- Edit form start -->
+                            <form id="quickForm" method="post" action="php/user/update.php">
+                                <div class="card-body">
+                                    <input type="text" hidden name="userId" value="' . $_GET["id"] . '">
+                                    <div class="form-group">
+                                        <label for="exampleInputUSerName">User Name</label>
+                                        <input required type="text" name="userName" autocomplete="off"
+                                               class="form-control" id="exampleInputEmail1"
+                                               placeholder="Enter USer Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email address</label>
+                                        <input required type="email" autocomplete="off" name="userEmail"
+                                               class="form-control" id="exampleInputEmail1"
+                                               placeholder="Enter email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Password</label>
+                                        <input required type="password" autocomplete="off" name="userPassword"
+                                               class="form-control"
+                                               id="exampleInputPassword1" placeholder="Password">
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-warning">Submit</button>
+                                </div>
+                            </form>';
+                            } else {
+                                echo '<!--New form start -->
                             <form id="quickForm" method="post" action="php/user/insert.php">
                                 <div class="card-body">
                                     <div class="form-group">
@@ -72,7 +116,8 @@ include("header.php");
                                 <div class="card-footer">
                                     <button type="submit" class="btn btn-primary">Submit</button>
                                 </div>
-                            </form>
+                            </form>';
+                            } ?>
                         </div>
                         <!-- /.card -->
                     </div>
@@ -107,17 +152,26 @@ include("header.php");
                         </thead>
                         <tbody>
 
-                        <tr>
-                            <td>1</td>
-                            <td>Trident</td>
-                            <td>Internet</td>
+                        <?php
+                        $sql = "SELECT * FROM users";
+                        $result = $conn->query($sql);
+                        $count = 1;
+                        // output data of each row
+                        while ($row = $result->fetch_assoc()) {
+                            echo '<tr>
+                            <td>' . $count . '</td>
+                            <td>' . $row["name"] . '</td>
+                            <td>' . $row["email"] . '</td>
                             <td>
-                                <button class="btn btn-warning">Action</button>
+                            <a href="user?edit=true&id=' . $row["id"] . '" class="btn btn-warning">Edit</a>
                             </td>
                             <td>
-                                <button class="btn btn-danger">Action</button>
+                            <a href="php/user/delete?id=' . $row["id"] . '" class="btn btn-danger">Delete</a>
                             </td>
-                        </tr>
+                        </tr>';
+                            $count++;
+                        }
+                        ?>
 
                         </tbody>
                         <tfoot>
